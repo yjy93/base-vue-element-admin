e4cc0277fa3a3691d856797f5a5668ff890259ab
Gene <Genejob@163.com>
2020-08-31 17:00:17 +0800

    - 添加 element ui

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <template>
     <div id="app">
         <h1>Hello, base-vue-element-admin!</h1>
-        <h2>{{$t('action.name')}}</h2>
-        <h2>{{$t('action.name')}}</h2>
-        <el-button type="primary">132</el-button>
+
     </div>
 </template>
 
@@ -12,20 +10,37 @@
 
     export default {
         name: 'App',
-        components: {},
+        components: {
+        },
+        data() {
+            return {
+                tableData: { // 表格数据
+                    content: [{id: 1, name: 'Gene'}]
+                },
+                columns: [], // 表格列
+                extensibleBtn: {
+                    add: 'add'
+                }
+            }
+        },
+        methods: {
+            initColumns() {
+                this.columns = [
+                    {prop: "id", label: '数据列', minWidth: 100},
+                    {prop: "id", label: '我的数据列', minWidth: 100},
+                    {prop: "id", label: '我是据列', minWidth: 100},
+                    {prop: "id", label: '我是数据列', minWidth: 100},
+                    {prop: "id", label: '我是列', minWidth: 100},
+                    {prop: "id", label: '我是表据列', minWidth: 100},
+                    {prop: "id", label: '我是表数据列', minWidth: 100},
+                ]
+            }
+        },
         mounted() {
-            console.log('this =---->>',this.$store.state.name);
-            console.log(`%c $i18n`,"color:red",this.$i18n);
-            console.log(`%c $i18n`,"color:red",this.$t);
-            // axios.post('/user/getUser', {
-            //     name: 'Gene'
-            // }).then((res) => {
-            //     console.log(' user 请求回来了 --->>>', res);
-            // })
-
-            // this.$api.user.getUser().then((res) => {
-            //     console.log(`%c app 请求`, "color:red", res);
-            // })
+            // console.log('this =---->>', this.$store.state.name);
+            // console.log(`%c $i18n`, "color:red", this.$i18n);
+            // console.log(`%c $i18n`, "color:red", this.$t);
+            this.initColumns()
         }
     }
 </script>
